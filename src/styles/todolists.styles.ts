c0cb951427e0b0957024016025e92c02873beb53
AndriiPrysiazhniuk <sarutora81@gmail.com>
2025-04-21 23:03:27 +0300

    solved a little mistake

--- src/styles/todolists.styles.ts
+++ src/styles/todolists.styles.ts
@@ -3,6 +3,6 @@ import {SxProps} from "@mui/material";
 export const getListItemSx = (isDone: boolean): SxProps => ({
     p: 0,
     justifyContent: 'space-between',
-    textDecoration: 'line-through',
+    textDecoration: isDone ? 'line-through' : '',
     opacity: isDone ? 0.5 : 1,
 })
